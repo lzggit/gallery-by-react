a257a312f6fd3e0c12fab9e1bf4ad2b181cfa6e4
lzggit <291338715@qq.com>
2017-10-15 21:16:16 +0800

    init project structor

---
+++ src/components/Main.js
@@ -0,0 +1,37 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+//获取图片相关的数据
+let imageDatas = require('../data/imageDatas.json');
+
+//利用自执行函数(只执行一次)，将图片名信息转成图片URL路径信息.
+imageDatas = (function genImageURL(imageDatasArr){
+  for(let i=0;i<imageDatasArr.length;i++){
+    let singleImageData = imageDatasArr[i];
+
+    singleImageData.imageURL = require('../images'+singleImageData.fileName);
+    imageDatasArr[i] = singleImageData;
+  }
+  return imageDatasArr;
+})(imageDatas);
+
+//imageDatas = genImageURL(imageDatas);
+
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <section className="stage">
+        <section className="img-sec"></section>
+        <nav className="controller-nav"></nav>
+      </section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
