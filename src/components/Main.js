fc9ec9bf5368868184683754ef0c0fe645f7840e
lzggit <291338715@qq.com>
2017-10-16 08:18:29 +0800

    init project

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,34 +4,53 @@ require('styles/App.css');
 import React from 'react';
 
 //获取图片相关的数据
-let imageDatas = require('../data/imageDatas.json');
+var imageDatas = require('../data/imageDatas.json');
 
 //利用自执行函数(只执行一次)，将图片名信息转成图片URL路径信息.
 imageDatas = (function genImageURL(imageDatasArr){
-  for(let i=0;i<imageDatasArr.length;i++){
-    let singleImageData = imageDatasArr[i];
+  for(var i=0;i<imageDatasArr.length;i++){
+    var singleImageData = imageDatasArr[i];
 
-    singleImageData.imageURL = require('../images'+singleImageData.fileName);
+    singleImageData.imageURL = require('../images/'+singleImageData.fileName);
     imageDatasArr[i] = singleImageData;
   }
   return imageDatasArr;
 })(imageDatas);
 
-//imageDatas = genImageURL(imageDatas);
-
-
 class AppComponent extends React.Component {
   render() {
+      var imgFigures = [],
+          controllerUnits = [];
+      imageDatas.forEach((value, index) => {
+        imgFigures.push(<ImgFigure key={index} data={value}/>);
+      });
     return (
       <section className="stage">
-        <section className="img-sec"></section>
-        <nav className="controller-nav"></nav>
+        <section className="img-sec">
+          {imgFigures}
+        </section>
+        <nav className="controller-nav">
+          {controllerUnits}
+        </nav>
       </section>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
+class ImgFigure extends React.Component {
+  render() {
+    return(
+        <figure className="img-figure">
+          <img src={this.props.data.imageURL}
+              alt={this.props.data.title}/>
+          <figcaption>
+            <h2 className="img-title">{this.props.data.title}</h2>
+          </figcaption>
+        </figure>
+    );
+
+}
+}
+
 
 export default AppComponent;
